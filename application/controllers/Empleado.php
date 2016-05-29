805a03028be721d23783d7f3edf33f10e12216ed
jdosornio <donaldo.osornio@uabc.edu.mx>
2016-05-29 14:20:36 -0700

    Se modificó el nombre de los archivos para que funcionara con Linux

--- application/controllers/Empleado.php
+++ application/controllers/Empleado.php
@@ -1,7 +1,7 @@
 <?php
 if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class empleado extends CI_Controller
+class Empleado extends CI_Controller
 {
     public function __construct()
     {
