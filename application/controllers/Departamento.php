71695f82e7d6a8693ac962079548727ee8015726
Alex Herrera <herrera.luis@uabc.edu.mx>
2016-05-31 05:58:57 -0700

    Se agregó CRUD de catalago Departamento

---
+++ application/controllers/Departamento.php
@@ -0,0 +1,118 @@
+<?php
+if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Departamento extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('base');
+    }
+
+    public function index() {
+
+        //pagination settings
+        $config['base_url'] = base_url('departamento/index');
+        $config['total_rows'] = $this->base->count_rows('departamento');
+        $config['per_page'] = 15;
+        $config["uri_segment"] = 3;
+        $choice = $config["total_rows"] / $config["per_page"];
+        $config["num_links"] = floor($choice);
+
+        //config for bootstrap pagination class integration
+        $config['full_tag_open'] = '<ul class="pagination">';
+        $config['full_tag_close'] = '</ul>';
+        $config['first_link'] = false;
+        $config['last_link'] = false;
+        $config['first_tag_open'] = '<li>';
+        $config['first_tag_close'] = '</li>';
+        $config['prev_link'] = '&laquo';
+        $config['prev_tag_open'] = '<li class="prev">';
+        $config['prev_tag_close'] = '</li>';
+        $config['next_link'] = '&raquo';
+        $config['next_tag_open'] = '<li>';
+        $config['next_tag_close'] = '</li>';
+        $config['last_tag_open'] = '<li>';
+        $config['last_tag_close'] = '</li>';
+        $config['cur_tag_open'] = '<li class="active"><a href="#">';
+        $config['cur_tag_close'] = '</a></li>';
+        $config['num_tag_open'] = '<li>';
+        $config['num_tag_close'] = '</li>';
+
+        $this->pagination->initialize($config);
+        $data['page'] = ($this->uri->segment(3)) ? $this->uri->segment(3) : 0;
+
+        $result = $this->base->paginate('departamento', $config["per_page"], $data['page']);
+        $data['departamentos'] = $result;
+
+        $data['pagination'] = $this->pagination->create_links();
+
+        //Cargar menu, view y footer
+        $this->load->view('menu', array('title' => 'departamento'));
+        $this->load->view('departamento/departamento_view',$data);
+        $this->load->view('footer');
+    }
+
+    function update($id){
+        $data['id'] = $id;
+
+        //obtener registro de departamento
+        $data['departamento'] = $this->base->get('departamento', array('id' => $id))[0];
+
+        $this->form_validation->set_rules('nombre', 'Nombre', 'trim|required');
+
+        if ($this->form_validation->run() == FALSE){
+            //fail validation
+            $this->load->view('menu', array('title' => 'Modificar departamento'));
+            $this->load->view('departamento/departamento_update_view', $data);
+            $this->load->view('footer');
+
+        }else{
+            //pass validation
+            $data = array(
+                'nombre' => $this->input->post('nombre')
+            );
+
+            //update employee record
+            $this->base->update('departamento', array('id' => $id), $data);
+
+            //display success message
+            $this->session->set_flashdata('msg', '<div class="alert alert-success text-center">departamento actualizado exitosamente</div>');
+
+            redirect('departamento');
+        }
+    }
+
+    function add() {
+
+        //set validation rules
+        $this->form_validation->set_rules('nombre', 'Nombre', 'trim|required');
+
+        if ($this->form_validation->run() == FALSE){
+            //fail validation
+            $this->load->view('menu', array('title' => 'Nuevo departamento'));
+            $this->load->view('departamento/departamento_insert_view');
+            $this->load->view('footer');
+
+        } else {
+            //pass validation
+            $data = array(
+                'nombre' => $this->input->post('nombre')
+            );
+
+            //insert the form data into database
+            $this->base->add('departamento', $data);
+
+            //display success message
+            $this->session->set_flashdata('msg', '<div class="alert alert-success text-center">Departamento registrado exitosamente</div>');
+
+            redirect('departamento');
+        }
+    }
+
+    public function delete($id){
+
+        $this->base->delete('departamento', array('id' => $id));
+        redirect('departamento');
+    }
+}
---
+++ application/views/departamento/departamento_insert_view.php
@@ -0,0 +1,42 @@
+        <div id="content">
+
+            <div class="container">
+                <div class="row">
+                    <h1>Departamentos</h1>
+                </div>
+                <div class="row">
+                    <div class="col-sm-offset-3 col-lg-6 col-sm-6 well">
+                        <legend>Registro de departamentos</legend>
+
+                        <?php
+                        $attributes = array("class" => "form-horizontal", "id" => "departamentoform", "name" => "departamentoform");
+                        echo form_open("departamento/add", $attributes);?>
+                        <fieldset>
+
+                            <div class="form-group">
+                                <div class="row colbox">
+                                    <div class="col-lg-4 col-sm-4">
+                                        <label for="nombre" class="control-label">Nombre</label>
+                                    </div>
+                                    <div class="col-lg-8 col-sm-8">
+                                        <input id="nombre" name="nombre" placeholder="Nombre del departamento" type="text" class="form-control"  value="<?php echo set_value('nombre'); ?>" />
+                                        <span class="text-danger"><?php echo form_error('nombre'); ?></span>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <div class="col-sm-offset-4 col-lg-8 col-sm-8 text-left">
+                                    <input id="btn_add" name="btn_add" type="submit" class="btn btn-primary" value="Enviar" />
+                                    <input id="btn_cancel" name="btn_cancel" type="reset" class="btn btn-danger" value="Cancelar" onClick="location.href='<?php echo base_url('departamento'); ?>'"/>
+                                </div>
+                            </div>
+                        </fieldset>
+
+                        <?php echo form_close(); ?>
+                        <?php echo $this->session->flashdata('msg'); ?>
+                    </div>
+                </div>
+            </div>
+
+        </div><!-- #content -->
